8df63203022ab6b513012ceecb6cb51460f9a6df
Jonathan <52434574+JonathanSaan@users.noreply.github.com>
2021-12-31 11:02:52 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,11 +2,18 @@ const botao = document.querySelector('button');
 
 function verifique(verif) {
   verif.preventDefault()
-  var email = document.getElementById('email');
+  let email = document.querySelector('#email');
+  let senha = document.querySelector('#senha');
   if (email.value == "") {
     email.setAttribute('style', 'border-color: red');
   } else {
     email.removeAttribute('style', 'border-color: red');
+  };
+
+  if (senha.value == "") {
+    senha.setAttribute('style', 'border-color: red');
+  } else {
+    senha.removeAttribute('style', 'border-color: red');
   };
 };
 botao.addEventListener("click", verifique);
